eceece7ea0ac504790557b7b82495fb296aef614
Fernando Caudillo Tafoya <fcaudillotafoya@icloud.com>
2025-11-07 20:30:36 -0600

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Fernando Caudillo Tafoya on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
